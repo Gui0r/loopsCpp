081fe3313e6c4073fe029f552fc777d86abc657f
03398351093 <03398351093@senacrs.edu.br>
2024-04-23 20:16:10 -0300

    Exercicio 1 logica

--- ex1f.cpp
+++ ex1f.cpp
@@ -5,12 +5,15 @@
 using namespace std;
 int main(){
     setlocale(LC_ALL, "pt_BR.UTF-8");
-    int num;
+    int num, num2;
 
     cout << "Digite um número: ";
     cin >> num;
 
-    for(int i = 1; num > i; i+2){
+    cout << "Digite outro número maior que o primeiro número digitado: ";
+    cin >> num2;
+
+    for(int i = 1;i > num && i <= num2; i+=2){
         cout << i;
     }
 }
